a174cdfa1a2ac3be9e8cffad9b1e899ea73a11bf
vlevi34898 <szil.levente10@gmail.com>
2025-12-12 11:25:17 +0100

    feladat2.py

--- feladat2.py
+++ feladat2.py
@@ -1,9 +1,8 @@
 #2. feladat
-#írj egy programot, amely kiírja csökkenö sorrnedben írja ki a
+#írj egy programot, amely kiírja csökkenő sorrendben írja ki a
 #számokat 1 és 10 között.
 
 ciklusváltozó = 11
 while ciklusváltozó > 1:
     ciklusváltozó = ciklusváltozó - 1
     print(ciklusváltozó)
-    
