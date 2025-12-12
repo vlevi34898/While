c225886d726c2c7d6efcc5447ea3b3a3f29e616a
vlevi34898 <szil.levente10@gmail.com>
2025-12-12 11:25:40 +0100

    feladat1.py

--- feladat1.py
+++ feladat1.py
@@ -1,6 +1,8 @@
 #1.feladat
 #írj egy programot, amely kiírja a páros számokat 1 és 10 között.
+
 ciklusváltozó = 0
 while ciklusváltozó < 10:
     ciklusváltozó = ciklusváltozó + 2
     print(ciklusváltozó)
+
